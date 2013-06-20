580dec8d52c139b9483f0c4acca6794f26b59dc9
chr.vollmer@gmail.com <chr.vollmer@gmail.com@845c31db-4e74-12ea-3c1d-e205d8062f7b>
2013-06-20 09:34:23 +0000

    - m: renamed unit tests to be autosdiscoverable

--- pyhmmtb/tests/stats/test_misc.py
+++ pyhmmtb/tests/stats/test_misc.py
@@ -11,19 +11,19 @@ from pyhmmtb.stats.misc import sample_discrete
 class SamplediscreteTest(unittest.TestCase):
     
     def testRow(self):
-        assert sample_discrete([0.8, 0.2], 1, 10).shape == (1,10)
+        assert sample_discrete(np.array([0.8, 0.2]), 1, 10).shape == (1,10)
     
     def testDistSmall(self):
         n = 10000
         M = np.zeros((n,2))
         for i in range(n):
-            M[i,:] = sample_discrete([0.8, 0.1, 0.1], 1, 2)
+            M[i,:] = sample_discrete(np.array([0.8, 0.1, 0.1]), 1, 2)
         dist = np.bincount(M.ravel().astype(int)) / (n*2.)
         assert np.all(np.abs(dist-[0.8, 0.1, 0.1]) < 1e-2)
         
     def testDistLarge(self):
         n = 10000
-        M = sample_discrete([0.8, 0.2], n, 10)
+        M = sample_discrete(np.array([0.8, 0.2]), n, 10)
         dist = np.bincount(M.ravel().astype(int)) / (n*10.)
         assert np.all(np.abs(dist-[0.8, 0.2]) < 1e-2)
         
